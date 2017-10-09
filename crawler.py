cb273cb066e4f334987425183f7c1b1c9c997a63
mpuk <puk.matej@gmail.com>
2017-10-09 11:33:55 +0200

    imports; crawling through links and finding links

--- crawler.py
+++ crawler.py
@@ -1,12 +1,34 @@
+from urllib2 import urlopen
+from urlparse import urljoin
+from time import sleep
+from bs4 import BeautifulSoup
+from re import compile
+
+
 class Crawler(object):
     def __init__(self, start_url):
         self.visited_urls = set()
         self.unvisited_urls = []
+        self.base_url = 'http://www.imdb.com/'
+        self.base_url_regexp = compile(self.base_url + '*')
 
         self.unvisited_urls.append(start_url)
 
     def crawl(self):
-        pass
+        for url in self.unvisited_urls:
+            self.visited_urls.add(url)
+            response = urlopen(url)
+
+            soup = BeautifulSoup(response.read(), 'lxml')
+            links_soup = soup('a')
+            new_urls = set()
+
+            for link in links_soup:
+                if 'href' in link.attrs:
+                    pass
+                    # print urljoin(self.base_url, link['href'])
+                    # link_joined = urljoin(link, )
+                    # if self.base_url.match()
 
 
 if __name__ == '__main__':
