33470145065c53bfe2fa4f200ae2b3d8ef55b73d
mpuk <puk.matej@gmail.com>
2017-10-09 13:56:42 +0200

    added retrievieng and stroing movie urls; limit for numberof stored movie urls

--- crawler.py
+++ crawler.py
@@ -11,29 +11,45 @@ class Crawler(object):
         self.unvisited_urls = []
         self.base_url = 'http://www.imdb.com/'
         self.base_url_regexp = compile(self.base_url + '*')
+        self.movie_url_regexp = compile(self.base_url + 'title/tt[0-9]*')
+        self.movie_urls = set()
 
         self.unvisited_urls.append(start_url)
 
-    def crawl(self):
+    def crawl(self, limit):
         for url in self.unvisited_urls:
+            print "Crawling..."
             self.visited_urls.add(url)
             response = urlopen(url)
 
             soup = BeautifulSoup(response.read(), 'lxml')
             links_soup = soup('a')
-            new_urls = set()
 
             for link in links_soup:
                 if 'href' in link.attrs:
                     url_joined = urljoin(self.base_url, link['href'])
                     if self.base_url_regexp.match(url_joined) is not None:
-                        pass
-                    # link_joined = urljoin(link, )
-                    # if self.base_url.match()
+                        if (url_joined not in self.visited_urls and
+                                url_joined not in self.unvisited_urls):
 
-            sleep(3)
+                            self.unvisited_urls.append(url_joined)
+
+                            if self.movie_url_regexp.match(url_joined) is not None:
+                                movie_url = self.movie_url_regexp.search(url_joined)
+
+                                if movie_url is not None:
+                                    self.movie_urls.add(movie_url.group(0))
+
+                                if len(self.movie_urls) == limit:
+                                    for m in self.movie_urls:
+                                        print m
+                                    exit()
+
+            print "Sleeping..."
+            sleep(5)
 
 
 if __name__ == '__main__':
+    print "Started crawling..."
     c = Crawler('http://www.imdb.com/genre/')
-    c.crawl()
+    c.crawl(20)
