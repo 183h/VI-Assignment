e7b1872848efab58b4f8c3cb11a79fc9744dc6e3
mpuk <puk.matej@gmail.com>
2017-10-12 15:35:47 +0200

    added imports

--- parser.py
+++ parser.py
@@ -1,7 +1,10 @@
+from urllib2 import urlopen
+from time import sleep
+from bs4 import BeautifulSoup
+
 class Parser(object):
     def __init__(self, movies):
         self.movies = movies
 
     def parse():
         pass
-        
