dfd682c11bb475a15d4b1f93c00b95b975191469
mpuk <puk.matej@gmail.com>
2017-10-13 18:40:52 +0200

    opening/creation of files in parse method and iterating through movies

--- parser.py
+++ parser.py
@@ -1,10 +1,23 @@
 from urllib2 import urlopen
 from time import sleep
 from bs4 import BeautifulSoup
+import os
+
 
 class Parser(object):
     def __init__(self, movies):
         self.movies = movies
+        self.cwd = os.getcwd() + "/movies/"
 
-    def parse():
-        pass
+    def parse(self):
+        for movie in self.movies:
+            title_id = movie.split("/")[4]
+            file_path = self.cwd + title_id
+
+            if os.path.isfile(file_path):
+                file = open(file_path, "r")
+            else:
+                file = open(file_path, "w")
+                response = urlopen(movie)
+
+                file.write(response.read())
