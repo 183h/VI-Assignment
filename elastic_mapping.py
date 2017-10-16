d57a40351d791c4b1605456ca4e555b0d2e42b21
mpuk <puk.matej@gmail.com>
2017-10-16 19:59:56 +0200

    enclosed mapping in function

--- elastic_mapping.py
+++ elastic_mapping.py
@@ -3,26 +3,32 @@ from elasticsearch_dsl.connections import connections
 
 connections.create_connection(hosts=['localhost'])
 
-m = Mapping('movie')
+def create_mapping():
+    print "Creating mapping..."
 
-m.field('name', 'text')
-m.field('plot', 'text')
-m.field('genres', 'text')
-m.field('director', 'text')
-m.field('keywords', 'text')
-m.field('awards', 'text')
-m.field('stars', 'text')
-m.field('duration', 'text')
-m.field('actors', 'text')
-m.field('creators', 'text')
-m.field('description', 'text')
+    m = Mapping('movie')
 
-m.field('rating_value', 'float')
-m.field('rating_count', 'integer')
+    m.field('name', 'text')
+    m.field('plot', 'text')
+    m.field('genres', 'text')
+    m.field('director', 'text')
+    m.field('keywords', 'text')
+    m.field('awards', 'text')
+    m.field('stars', 'text')
+    m.field('duration', 'text')
+    m.field('actors', 'text')
+    m.field('creators', 'text')
+    m.field('description', 'text')
 
-m.field('language', 'keyword')
-m.field('country', 'keyword')
+    m.field('ratingValue', 'float')
+    m.field('ratingCount', 'integer')
 
-m.field('release_date', 'date')
+    m.field('language', 'keyword')
+    m.field('country', 'keyword')
 
-m.save('imdb')
+    m.field('releaseDate', 'date')
+
+    m.save('imdb')
+
+if __name__ == '__main__':
+    create_mapping()
