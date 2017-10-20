62ee9f2f482860b4df3f26c42d94df5be250b358
mpuk <puk.matej@gmail.com>
2017-10-20 21:23:19 +0200

    formating and creating aggs filter for search query

--- app.py
+++ app.py
@@ -1,65 +1,76 @@
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
-import os
 from json import dumps
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
-    return render_template('main.html', o="desc")
+    return render_template('main.html', o="asc")
+
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
-    q = request.form.get('q', default = "", type = str)
-    o = request.form.get('o', default = "desc", type = str)
+    q = request.form.get('q', default="", type=str)
+    o = request.form.get('o', default="desc", type=str)
+    a = request.form.get('a', default=None, type=str)
     es = Elasticsearch()
+    aggs_filter = {}
 
-    res = es.search(index="imdb", doc_type="movie",
-        body={
-            "sort" : [
-                { 
-                    "ratingValue" : {
-                        "order" : o
-                    }
-                },
-                "_score"
-            ],
-            "query" : {
-                "bool" : {
-                    "must" : {
-                        "multi_match" : {
-                            "query" : q,
-                            "type" : "most_fields",
-                            "fields" : ["name", "description", "plot", "actors", "stars"]
-                        }
-                    }
-                }
-            },
-            "highlight" : {
-                "pre_tags" : ['<span style="background-color: yellow">'],
-                "post_tags" : ["</span>"],
-                "fields" : {
-                    '*' : {}
-                }
-            },
-            "aggs" : {
-                "by_language" : {
-                    "terms" : {
-                        "field" : "country",
-                        "size" : 5
-                    }
-                }
+    if a is not None:
+        aggs_filter = {
+            "term": {
+                'country': a
             }
         }
-    )
+        print aggs_filter
 
-    print dumps(res, indent=4)
-    print q, o
+    res = es.search(index="imdb", doc_type="movie",
+                    body={
+                        "sort": [
+                            {
+                                "ratingValue": {
+                                    "order": o
+                                }
+                            },
+                            "_score"
+                        ],
+                        "query": {
+                            "bool": {
+                                "must": {
+                                    "multi_match": {
+                                        "query": q,
+                                        "type": "most_fields",
+                                        "fields": ["name", "description", "plot", "actors", "stars"]
+                                    }
+                                },
+                                "filter": aggs_filter
+                            }
+                        },
+                        "highlight": {
+                            "pre_tags": ['<span style="background-color: yellow">'],
+                            "post_tags": ["</span>"],
+                            "fields": {
+                                '*': {}
+                            }
+                        },
+                        "aggs": {
+                            "by_language": {
+                                "terms": {
+                                    "field": "country",
+                                    "size": 10
+                                }
+                            }
+                        }
+                    }
+                    )
+
+    # print dumps(res, indent=4)
+    print q, o, a
 
     return render_template(
         'hits.html',
-         data=res["hits"]["hits"]
-         , q=q, o=o,
-          aggs=res["aggregations"]["by_language"]["buckets"]
+        data=res["hits"]["hits"], q=q, o=o,
+        aggs=res["aggregations"]["by_language"]["buckets"]
     )
